f0319d2c470f938c79fb6fdc0471a8a60d53a36b
Vagner Mello <vagnerjsmello@gmail.com>
2020-06-12 15:47:35 +0100

    ASP.NET MVC Globalization

---
+++ ProjectGlobalizationApp/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProjectGlobalizationApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index(string culture)
+        {
+            if (!string.IsNullOrEmpty(culture))
+            {
+                HttpContext.Session["culture"] = culture;
+                Thread.CurrentThread.CurrentUICulture = new CultureInfo(culture);
+                return View();
+            }
+
+            culture = string.IsNullOrEmpty((string)Session["culture"]) ? "pt-br" : Session["culture"].ToString();
+            Thread.CurrentThread.CurrentUICulture = new CultureInfo(culture);
+
+            return View();
+        }
+    }
+}
